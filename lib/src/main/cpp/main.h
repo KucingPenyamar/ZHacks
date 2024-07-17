6f39d30623cd7a180e0dec0b2e863d8c6ce2c678
KucingPenyamar <christetopan@gmail.com>
2024-07-17 22:34:55 +0700

    update lib/src/main/cpp/main.h

--- lib/src/main/cpp/main.h
+++ lib/src/main/cpp/main.h
@@ -5,4 +5,8 @@
 const char *targetProcessName = "com.unity.test";
 void inject(const char *gameDataDir, void *data, size_t length);
 
+#define hook_input(ret, func, ...) \
+    ret (*orig##func)(__VA_ARGS__); \
+    ret my##func(__VA_ARGS__)
+
 #endif
