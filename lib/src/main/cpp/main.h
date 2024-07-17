90e4e6b409e13f8f9d676762668d58b47f00e291
KucingPenyamar <christetopan@gmail.com>
2024-07-17 10:29:06 +0700

    update lib/src/main/cpp/main.h

--- lib/src/main/cpp/main.h
+++ lib/src/main/cpp/main.h
@@ -1,7 +1,7 @@
 #ifndef DIRECTIVE_STATEMENT
 #define DIRECTIVE_STATEMENT
 
-void inject(const char *gameDataDir);
 const char *targetProcessName = "com.unity.test";
-
+void inject(const char *gameDataDir);
+void logger(const char *outDir)
 #endif
