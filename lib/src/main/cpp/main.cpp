466cc71277ae50fc346551d380b0a5027d934ffd
KucingPenyamar <christetopan@gmail.com>
2024-07-17 11:37:01 +0700

    Fix error: Undefined types

--- lib/src/main/cpp/main.cpp
+++ lib/src/main/cpp/main.cpp
@@ -147,7 +147,7 @@ void inject(const char *gameDataDir) {
     DobbyHook(sym_addr, (dobby_dummy_func_t)fake_##name, (dobby_dummy_func_t *)&orig_##name);            
      */
     // HOOK INPUT SYMBOL
-    input_handler = DobbySymbolResolver("libinput.so", "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
+    auto input_handler = DobbySymbolResolver("libinput.so", "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
     DobbyHook((void *)input_handler, (void *)inputHook, (void**)inputOrig);
     // HOOK EGLSWAPBUFFER
     DobbySymbolResolver("libEGL.so", "eglSwapBuffers");
